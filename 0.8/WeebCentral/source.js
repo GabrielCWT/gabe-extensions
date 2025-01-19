7ae9fb24c1f49a457bb4b5c0e3210419fb2dcd9b
GabrielCWT <77312579+GabrielCWT@users.noreply.github.com>
2025-01-19 00:24:43 +0000

    Deploying to gh-pages from @ GabrielCWT/gabe-extensions@b8c9fe0eefe2deee71a2cc3c3aff5d748e964751 🚀

--- 0.8/WeebCentral/source.js
+++ 0.8/WeebCentral/source.js
@@ -1440,7 +1440,7 @@ const types_1 = require("@paperback/types");
 const WeebCentralParser_1 = require("./WeebCentralParser");
 const BASE_DOMAIN = 'https://weebcentral.com';
 exports.WeebCentralInfo = {
-    version: '1.0.2',
+    version: '1.0.3',
     name: 'WeebCentral',
     description: 'Extension that pulls manga from WeebCentral.',
     author: 'Gabe',
@@ -1841,21 +1841,6 @@ class Parser {
         const recommendation = [];
         const hot = [];
         const recent = [];
-        for (const recommendationObj of $('.glide__slide:not(.glide__slide--clone)').toArray()) {
-            const id = $('a', recommendationObj).attr('href') ?? '';
-            const title = $('.text-white', recommendationObj).text().trim() ?? '';
-            const image = $('source', recommendationObj).first().attr('srcset') ??
-                $('img', recommendationObj).attr('src') ??
-                '';
-            recommendation.push(App.createPartialSourceManga({
-                image,
-                title: this.decodeHTMLEntity(title),
-                mangaId: id,
-                subtitle: '',
-            }));
-        }
-        recommendationSection.items = recommendation;
-        sectionCallback(recommendationSection);
         for (const hotObj of $('article.flex.gap-4', 'section.bg-base-200.max-w-7xl').toArray()) {
             const id = $('a', hotObj)
                 .first()
@@ -1895,6 +1880,25 @@ class Parser {
         }
         recentSection.items = recent;
         sectionCallback(recentSection);
+        for (const recommendationObj of $('.glide__slide:not(.glide__slide--clone)').toArray()) {
+            const id = $('a', recommendationObj)
+                .attr('href')
+                ?.replace(/\/$/, '')
+                ?.split('/')
+                .slice(-2)[0] ?? '';
+            const title = $('.text-white', recommendationObj).text().trim() ?? '';
+            const image = $('source', recommendationObj).first().attr('srcset') ??
+                $('img', recommendationObj).attr('src') ??
+                '';
+            recommendation.push(App.createPartialSourceManga({
+                image,
+                title: this.decodeHTMLEntity(title),
+                mangaId: id,
+                subtitle: '',
+            }));
+        }
+        recommendationSection.items = recommendation;
+        sectionCallback(recommendationSection);
     }
     parseViewMore($) {
         const manga = [];
