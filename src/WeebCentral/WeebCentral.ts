46b6ee6b119a347a8fa40b24d1aebb93daf8c3ba
GabrielCWT <77312579+GabrielCWT@users.noreply.github.com>
2025-02-27 16:46:34 +0800

    fix (WeebCentral): Fix pagination and tag search

--- src/WeebCentral/WeebCentral.ts
+++ src/WeebCentral/WeebCentral.ts
@@ -23,7 +23,7 @@ import { Parser } from './WeebCentralParser'
 const BASE_DOMAIN = 'https://weebcentral.com'
 
 export const WeebCentralInfo: SourceInfo = {
-    version: '1.0.6',
+    version: '1.0.7',
     name: 'WeebCentral',
     description: 'Extension that pulls manga from WeebCentral.',
     author: 'Gabe',
@@ -139,19 +139,17 @@ export class WeebCentral
         const LIMIT = 32
         const offset = metadata?.offset ?? 0
         let searchParams = ''
-        // Regular search
+        // Title search
         if (query.title) {
             searchParams = searchParams.concat(
                 encodeURI(`&text=${query.title ?? ''}`)
             )
         }
         // Tag search
-        else {
-            for (const tag of query.includedTags) {
-                searchParams = searchParams.concat(`&included_tag=${tag.id}`)
-            }
-            searchParams.concat(`limit=${LIMIT}&offset=${offset}`)
+        for (const tag of query.includedTags) {
+            searchParams = searchParams.concat(`&included_tag=${tag.id}`)
         }
+        searchParams = searchParams.concat(`&limit=${LIMIT}&offset=${offset}`)
         const request = App.createRequest({
             url: `${this.baseUrl}/search/data?sort=Best%20Match&order=Ascending&display_mode=Full%20Display${searchParams}`,
             method: 'GET',
