28793354a700b0c0cbc240d9ddcf1956879fab3b
GabrielCWT <77312579+GabrielCWT@users.noreply.github.com>
2025-03-15 14:48:14 +0000

    Deploying to gh-pages from @ GabrielCWT/gabe-extensions@277f5c869ce2d0678d38162af0910bfa15e695ef 🚀

--- 0.8/WeebCentral/source.js
+++ 0.8/WeebCentral/source.js
@@ -1440,7 +1440,7 @@ const types_1 = require("@paperback/types");
 const WeebCentralParser_1 = require("./WeebCentralParser");
 const BASE_DOMAIN = 'https://weebcentral.com';
 exports.WeebCentralInfo = {
-    version: '1.0.7',
+    version: '1.0.8',
     name: 'WeebCentral',
     description: 'Extension that pulls manga from WeebCentral.',
     author: 'Gabe',
@@ -1840,7 +1840,7 @@ class Parser {
         });
         const recentSection = App.createHomeSection({
             id: 'recent',
-            title: 'Recently Updated',
+            title: 'Latest Updates',
             type: types_1.HomeSectionType.singleRowNormal,
             containsMoreItems: true,
         });
@@ -1866,7 +1866,7 @@ class Parser {
         }
         hotSection.items = hot;
         sectionCallback(hotSection);
-        for (const recentObj of $('article', 'section.cols-span-1.rounded').toArray()) {
+        for (const recentObj of $('article', 'section.bg-base-200.rounded-sm').toArray()) {
             const id = $('a.aspect-square', recentObj)
                 .attr('href')
                 ?.replace(/\/$/, '')
